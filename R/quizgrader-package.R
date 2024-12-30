9df8c7e8f47bfe37cf6985c73502c242ed3a0dde
Andreas Handel <andreas.handel@gmail.com>
2024-12-30 15:11:39 -0500

    small update

---
+++ R/quizgrader-package.R
@@ -0,0 +1,18 @@
+#' quizgrader: A package to do grading
+#'
+#' This package allows easy setup and deployment of autograded online quizzes.
+#'
+#' Start the main menu of the package by calling quizmanager().
+#'
+#' To learn more about how to use the package, see the documentation
+#' on the package website:
+#' https://andreashandel.github.io/quizgrader/
+
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom utils globalVariables
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
